cfd1bddf52052259b908387bfb987464afa20ce0
AJ <ajanthan29894@gmail.com>
2020-04-11 16:02:20 +0100

    initial commit

---
+++ navigation/BottomTabNavigator.js
@@ -0,0 +1,89 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import * as React from "react";
+
+import TabBarIcon from "../components/TabBarIcon";
+import HomeScreen from "../screens/HomeScreen";
+import LinksScreen from "../screens/LinksScreen";
+
+const BottomTab = createBottomTabNavigator();
+const INITIAL_ROUTE_NAME = "Home";
+
+export default function BottomTabNavigator({ navigation, route }) {
+  // Set the header title on the parent stack navigator depending on the
+  // currently active tab. Learn more in the documentation:
+  // https://reactnavigation.org/docs/en/screen-options-resolution.html
+  navigation.setOptions({ headerTitle: getHeaderTitle(route) });
+
+  return (
+    <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
+      <BottomTab.Screen
+        name="Indices"
+        component={HomeScreen}
+        options={{
+          title: "Indices",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} name="md-code-working" />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="Funds"
+        component={LinksScreen}
+        options={{
+          title: "Funds",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} name="md-book" />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="Accounts"
+        component={LinksScreen}
+        options={{
+          title: "Accounts",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} name="md-book" />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="Links2"
+        component={LinksScreen}
+        options={{
+          title: "Resources",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} name="md-book" />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="News"
+        component={LinksScreen}
+        options={{
+          title: "News",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} name="md-book" />
+          ),
+        }}
+      />
+    </BottomTab.Navigator>
+  );
+}
+
+function getHeaderTitle(route) {
+  const routeName =
+    route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
+
+  switch (routeName) {
+    case "Indices":
+      return "Starred Indices";
+    case "Funds":
+      return "Your Funds";
+    case "Accounts":
+      return "Your Accounts";
+    case "Links2":
+      return "Your tab4 ";
+    case "News":
+      return "Stock News";
+  }
+}
